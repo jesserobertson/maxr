9f708d3bd56393692f83df52187c461c5708b3e9
Robertson, Jess (Mineral Resources, Kensington) <Jesse.Robertson@csiro.au>
2015-09-25 09:13:41 +0800

    Add blinking vortex flow

--- maxr/flow/__init__.py
+++ maxr/flow/__init__.py
@@ -1,4 +1,5 @@
 from .flow import Flow
 from .generators import from_function
+from .blink import blink
 
 __all__ = ['Flow', 'from_function']
--- maxr/flow/blink.py
+++ maxr/flow/blink.py
@@ -6,4 +6,47 @@
     description: Blinking vortex flow
 """
 
+from numpy import asarray, clip, pi, sin
 
+
+def vortex(x_center, y_center, gamma):
+    """ Returns a function to give a vortex flow centered at r,
+        with strength Gamma
+    """
+    def _flow(xxs, yys):
+        "Wrapped flow function"
+        xxs = asarray(xxs)
+        yys = asarray(yys)
+        rsq = (xxs - x_center) ** 2 + (yys - y_center) ** 2
+        return (gamma * (yys - y_center) / (2 * pi * rsq),
+                -gamma * (xxs - x_center) / (2 * pi * rsq))
+    return _flow
+
+
+def tick(time, period):
+    """ Adding some time dependence!
+    """
+    return clip(sin(2 * pi * time / period), 0, 1.01)
+
+
+def tock(time, period):
+    """ Adding some time dependence!
+    """
+    return clip(-sin(2 * pi * time / period), 0, 1.01)
+
+
+def blink(gamma, period):
+    """ The blinking vortex flow
+    """
+    # Generate two vortices
+    vortices = [vortex(x, 0, gamma) for x in (-0.5, 0.5)]
+
+    # Wrap in flow with time dependence
+    def _flow(xxs, yys, time):
+        "Wrapped flow function"
+        _vortex = [v(xxs, yys) for v in vortices]
+        return _vortex[0][0] * tick(time, period) \
+               + vortex[1][0] * tock(time, period), \
+               _vortex[0][1] * tick(time, period) \
+               + vortex[1][1] * tock(time, period)
+    return _flow
