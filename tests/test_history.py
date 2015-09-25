5b926186e6fb6d99411c2ab7739bac816d2541ab
Robertson, Jess (Mineral Resources, Kensington) <Jesse.Robertson@csiro.au>
2015-09-25 08:49:59 +0800

    Delinting

--- tests/test_history.py
+++ tests/test_history.py
@@ -4,49 +4,55 @@
 from __future__ import print_function, division
 
 import unittest
-import numpy
+from numpy import array, sqrt, pi, linspace, sin, cos, arange, median
 from scipy.special import fresnel
 from collections import defaultdict
 
 from maxr.integrator import history
 
 
-def solution(t):
+def solution(time):
     """ Solution to sinusoidal history term
     """
-    ssc, csc = fresnel(numpy.sqrt(2 * t / numpy.pi)) 
-    return numpy.sqrt(2 * numpy.pi) * (
-        csc * numpy.sin(t) - ssc * numpy.cos(t))
+    ssc, csc = fresnel(sqrt(2 * time / pi))
+    return sqrt(2 * pi) * (
+        csc * sin(time) - ssc * cos(time))
 
 
-def evaluate_history_integral(f, ts, order=1):
-    """ Evaluate the history integral for a given driving function f
+def evaluate_history_integral(func, times, order=1):
+    """ Evaluate the history integral for a given driving function func
     """
-    return numpy.array([0] + [
-        history.integrator(f(ts[:idx+1]), ts[:idx+1], order=order)
-        for idx in range(1, len(ts))])
+    return array([0] + [
+        history.integrator(func(times[:idx+1]), times[:idx+1],
+                           order=order)
+        for idx in range(1, len(times))])
 
 
 class TestHistory(unittest.TestCase):
-    
-    """ Tests to test integrator.history module
+
+    r""" Tests to test integrator.history module
 
         Check accuracy of convergence. We use a sinusoidal forcing and plot the response
-        $$ 
-        \int_0^{t} \frac{\sin{(\tau)}}{\sqrt{t - \tau}}d\tau = \sqrt{2 \pi}\left[C{\left(\sqrt{\frac{2t}{\pi}}\right)}\sin{t} - S{\left(\sqrt{\frac{2t}{\pi}}\right)}\cos{t}\right]
         $$
-        where $C$ is the Fresnel C (cos) integral, and $S$ is the Fresnel $S$ (sin) integral. Note the solution in the paper is **WRONG**
+        \int_0^{t} \frac{\sin{(\tau)}}{\sqrt{t - \tau}}d\tau =
+            \sqrt{2 \pi}\left[
+                C{\left(\sqrt{\frac{2t}{\pi}}\right)}\sin{t}
+                - S{\left(\sqrt{\frac{2t}{\pi}}\right)}\cos{t}
+            \right]
+        $$
+        where $C$ is the Fresnel C (cos) integral, and $S$ is the Fresnel 
+        $S$ (sin) integral. Note the solution in the paper is **WRONG**
     """
 
     tmin, tmax = 0, 30
-    
+
     def test_integrator(self):
         """ Integrator should function ok
         """
         expected = -1.2492166377597749
-        ts = numpy.linspace(self.tmin, self.tmax, 1000)
+        times = linspace(self.tmin, self.tmax, 1000)
         self.assertTrue(
-            history.integrator(numpy.sin(ts), ts) - expected < 1e-5)
+            history.integrator(sin(times), times) - expected < 1e-5)
 
     def test_integrator_solution(self):
         """ Integrator should work over a range of values
@@ -54,24 +60,23 @@ class TestHistory(unittest.TestCase):
         nsteps = 1280
         order = 3
         for tmax in (10, 20, 50):
-            ts = numpy.linspace(self.tmin, tmax, nsteps)
-            numeric = evaluate_history_integral(numpy.sin, ts, order=order)
-            exact = solution(ts)
+            times = linspace(self.tmin, tmax, nsteps)
+            numeric = evaluate_history_integral(sin, times, order=order)
+            exact = solution(times)
             self.assertTrue(
-                numpy.median(numeric - exact) < 1e-5)
+                median(numeric - exact) < 1e-5)
 
     def test_range_steps(self):
         """ Integrators should work for all orders at all steps
         """
         # Set up steps
         nstepstep = 25
-        nsteps = numpy.arange(nstepstep, 500, nstepstep)
-        spacing = 10 / (nsteps - 1)
+        nsteps = arange(nstepstep, 500, nstepstep)
 
         # Calculate error
         error = defaultdict(list)
         for order in (1, 2, 3):
-            for N in nsteps:
-                ts = numpy.linspace(self.tmin, self.tmax, N)
-                err = evaluate_history_integral(numpy.sin, ts, order=order) - solution(ts)
+            for num in nsteps:
+                times = linspace(self.tmin, self.tmax, num)
+                err = evaluate_history_integral(sin, times, order=order) - solution(times)
                 error[order].append(abs(err).max())
