569b20b949b129fe746f6f6f3ca90e1764c6897e
Robertson, Jess (Mineral Resources, Kensington) <Jesse.Robertson@csiro.au>
2018-06-20 09:46:16 +0800

    Adding versioneer

--- maxr/__init__.py
+++ maxr/__init__.py
@@ -2,3 +2,6 @@ from . import integrator
 from .parameters import Parameters
 
 __all__ = ['integrator', 'Parameters']
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
