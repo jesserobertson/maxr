4b14fd4da4365783bd182811d4acedc65dd72666
Robertson, Jess (Mineral Resources, Kensington) <Jesse.Robertson@csiro.au>
2018-06-20 09:46:53 +0800

    Updating setup to handle Cython extensions

--- setup.py
+++ setup.py
@@ -6,33 +6,55 @@
     description: Setuptools installer script for maxr.
 """
 
+# Check that our version of setuptools is new enough
+# Resolving Cython and numpy dependencies via 'setup_requires' requires setuptools >= 18.0:
+# https://github.com/pypa/setuptools/commit/a811c089a4362c0dc6c4a84b708953dde9ebdbf8
+import sys
+import textwrap
+import pkg_resources
+try:
+    pkg_resources.require('setuptools >= 18.0')
+except pkg_resources.ResolutionError:
+    print(textwrap.dedent("""
+        setuptools >= 18.0 is required, and the dependency cannot be
+        automatically resolved with the version of setuptools that is
+        currently installed (%s).
+
+        You can upgrade setuptools:
+        $ pip install -U setuptools
+        """ % pkg_resources.get_distribution("setuptools").version),
+        file=sys.stderr)
+    sys.exit(1)
+
+# Now we can do all our imports
 from setuptools import setup, find_packages
-import os
-
-def read(*paths):
-    """ Build a file path from *paths and return the contents.
-    """
-    with open(os.path.join(*paths), 'r') as f:
-        return f.read()
-
-# Get requirements from requirements.txt file
-with open('requirements.txt') as fhandle:
-    REQUIREMENTS = [l.strip('\n') for l in fhandle]
-
-# Get version number from _version.py
-# Can be updated using python setup.py update_version
-from update_version import update_version, Version, get_version
-update_version()
+from setup_extensions import get_extensions, get_cmdclass
+import versioneer
 
 ## PACKAGE INFORMATION
+# Get requirements from requirements.txt file
+with open('requirements.txt', 'r') as src:
+    REQUIREMENTS = [l.strip('\n') for l in src]
+
+# Long description from readme
+with open('README.md', 'r') as src:
+    LONG_DESCRIPTION = src.read()
+
+# Get extensions and update comandclass
+VERSION = versioneer.get_version()
+EXTENSIONS = get_extensions()
+CMDCLASS = versioneer.get_cmdclass()
+CMDCLASS.update(get_cmdclass())
+
+# Actually configure setuptools
 setup(
-    # Metadata
     name='maxr',
-    version=get_version(),
+    version=VERSION,
     description='High-order integration for the Maxey-Riley equations',
+    long_description=LONG_DESCRIPTION,
     author='Jess Robertson',
     author_email='jesse.robertson@csiro.au',
-    url='http://github.com/jesserobertson/maxr',
+    url='ssh://git@bitbucket.csiro.au:7999/rose/hyper.git',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -51,12 +73,26 @@ setup(
 
     # Dependencies
     install_requires=REQUIREMENTS,
+    setup_requires=[
+        'pytest-runner',
+        'Cython >= 0.20',
+        'numpy'
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+    ],
 
     # Contents
-    packages=find_packages(exclude=['test*']),
-    package_data={},
-    test_suite='tests',
-    cmdclass={
-        'update_version': Version
-    }
+    packages=find_packages(
+        exclude=['tests', 'jupyter', 'resources']
+    ),
+    test_suite="tests",
+    package_data={
+        # 'hyper.scalars': ['*.json']
+    },
+
+    # Cython extensions & other stuff
+    cmdclass=CMDCLASS,
+    ext_modules=EXTENSIONS
 )
